cbaabe16ee2facda7b605e82589b9fb0f55534b8
zraq301-lgtm <zraq301@gmail.com>
2026-05-01 13:12:05 +0300

    Simplify App component to render Dashboard directly
    Removed routing and simplified App component to render only the Dashboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import InventoryList from "./pages/Inventory/InventoryList.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/inventory" element={<InventoryList />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Dashboard />;
 }
 
 export default App;
