0574684f13a953338badeada4527e38aa697b1b0
zraq301-lgtm <zraq301@gmail.com>
2026-05-01 12:36:19 +0300

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../components/layout/Layout.jsx";
 
-import Dashboard from "../pages/Dashboard/Dashboard.jsx";
-import InventoryList from "../pages/Inventory/InventoryList.jsx";
+function Dashboard() {
+  return <h1>لوحة التحكم تعمل 🎉</h1>;
+}
 
-function AppRoutes() {
+function Inventory() {
+  return <h1>المخزون يعمل 🎉</h1>;
+}
+
+export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/inventory" element={<InventoryList />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
-export default AppRoutes;
