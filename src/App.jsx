6336427ec5d3682eba7a2ebf34a478ba0957c8f3
zraq301-lgtm <zraq301@gmail.com>
2026-05-01 13:08:32 +0300

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import AppRoutes from "./routes/AppRoutes.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+import InventoryList from "./pages/Inventory/InventoryList.jsx";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/inventory" element={<InventoryList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
